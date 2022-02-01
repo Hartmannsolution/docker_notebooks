5bddd84a83edbe93f07ef65baebc75e821f1971e
Thomas Hartmann <thomas@webtrade.dk>
2022-02-01 14:13:19 +0100

    fixed notebook: intro

---
+++ notebooks/my_work/modules/main.py
@@ -0,0 +1,5 @@
+import my_first_py_file as mf 
+
+
+if __name__ == '__main__':
+    mf.m
---
+++ notebooks/my_work/modules/my_first_py_file.py
@@ -0,0 +1,7 @@
+def my_func():
+    print("Hello from func")
+    
+print("hello from indentation level 0")
+
+if __name__ == '__main__':
+    my_func()
