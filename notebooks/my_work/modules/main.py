dd481ac4f725433f4815f975b5a1182228fc12f6
Thomas Hartmann <thomas@webtrade.dk>
2022-02-01 15:31:26 +0100

    cleanup

--- notebooks/my_work/modules/main.py
+++
@@ -1,5 +0,0 @@
-import my_first_py_file as mf 
-
-
-if __name__ == '__main__':
-    mf.m
--- notebooks/my_work/modules/my_first_py_file.py
+++
@@ -1,7 +0,0 @@
-def my_func():
-    print("Hello from func")
-    
-print("hello from indentation level 0")
-
-if __name__ == '__main__':
-    my_func()
